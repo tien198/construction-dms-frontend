06396a6d6c237b8ec639583d36b4710939d705e6
tien <tienvn998@gmail.com>
2026-05-11 22:06:52 +0700

    update

--- src/pages/construction/kq-kh-lcnt/actions/edit-action.ts
+++ src/pages/construction/kq-kh-lcnt/actions/edit-action.ts
@@ -1,9 +1,13 @@
 import type { ActionFunctionArgs } from "react-router";
-import { edit_kq_kh_lcnt_store } from "../store/edit-store";
+import { edit_tv_store, edit_tt_store } from "../store/edit-store";
+import { POST_ADD_SUBMISSION } from "@/lib/api-list/document-api-list";
 
-export function editKqKhLcntAction(args: ActionFunctionArgs) {
-  const state = edit_kq_kh_lcnt_store.getState();
-  const submission = state.submission;
+export async function editKqKhLcntAction(args: ActionFunctionArgs) {
+  const conId = args.params["con-id"];
+
+  const tv_state = edit_tv_store.getState();
+  const tt_state = edit_tt_store.getState();
+  const submission = { ...tv_state.submission, ...tt_state.submission };
   console.log(submission, args);
 
   return null;
